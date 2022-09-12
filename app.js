1ba06ceb694f94254d8d8dd429b46061ecacfead
Rodolfo <ultrabyte14@hotmail.com>
2022-09-11 23:25:39 -0600

    new start validate empty fields added

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let OnCard = document.querySelector('h1');
 
 cardNumber.addEventListener('input', function () {
     OnCard.innerText = this.value;
-    console.log(OnCard);
 });
 
 // change card name dynamically on CARD image
@@ -40,4 +39,53 @@ let addCVC = document.querySelector('div.cvc>p');
 getCVC.addEventListener('input', function () {
     addCVC.innerText = this.value;     // add the typed in value to the <p> on the card image
 });
+
+
+// Add event listernert to button  when is clicked, to validate and if all data is correct
+// hide the current form and display the final status Thank you on screen 
+
+let confirmBTN = document.querySelector('#confirm');
+
+// validate that the input ntype name is not empty.
+confirmBTN.addEventListener('click', function () {
+    let verifyName = document.querySelector('.nameWarning').classList;
+    let veryfyCard = document.querySelector('.numberWarning').classList;
+    let veryfyDate = document.querySelector('.dateWarning').classList;
+    let veryfyCVC = document.querySelector('.cvcWarning').classList;
+    let emptyName = "";
+
+    if (input_Name.value == "") {
+        input_Name.value = emptyName;
+        checkField(verifyName, input_Name, emptyName);
+    }
+
+    if (cardNumber.value == "") {
+        checkField(veryfyCard, cardNumber, emptyName);
+    }
+
+    if (get_month_value.value == "" || get_year_value.value == "") {
+        checkField(veryfyDate, get_month_value, emptyName);
+        checkField(veryfyDate, get_year_value, emptyName);
+    }
+
+    if (getCVC.value == "") {
+        checkField(veryfyCVC, getCVC, emptyName);
+    }
+
+});
+
+// validate the field is not empty. using a switch statement
+
+function checkField(param1, param2, param3) {
+    switch (param2.value) {
+        case param3:
+            param1.replace('hide', 'show');
+            break;
+
+        default:
+            console.log(param2.value);
+            break;
+    }
+}
+
 
